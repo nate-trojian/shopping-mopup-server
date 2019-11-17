610d2225051ae4ab4f45728d9b3573364f28fe33
nate-trojian <ntrojian@gmail.com>
2019-11-17 12:22:17 -0500

    Add trial lists

--- src/index.js
+++ src/index.js
@@ -8,40 +8,112 @@ import App from './App';
 
 const player1Items = {
   "Apple Pie": {
-    "Apples": null,
-    "Crust": {
-      "Milk": null,
-      "Flour": null
+    "Pie Crust": {
+      "Flour": null,
+      "Butter": null
     },
+    "Apples": null,
     "Sugar": null
   },
-  "Chicken Pot Pie": {
-    "Chicken": null,
-    "Crust": {
-      "Milk": null,
-      "Flour": null
+  "Lasagna": {
+    "Pasta": null,
+    "Tomato Sauce": {
+      "Tomatoes": null,
+      "Garlic": null
     },
-    "A Pot": null
+    "Cheese": null,
+    "Lasagna Filling": {
+      "Ground Beef": null,
+      "Spinach": null
+    }
   },
+  "Tuna Casserole": {
+    "Canned Tuna": null,
+    "Pasta": null,
+    "Onions": null
+  },
+  "Egg Salad Sandwich": {
+    "Bread": null,
+    "Eggs": null,
+    "Mayonnaise": null
+  },
+  "Sloppy Joes": {
+    "Ground Beef": null,
+    "Tomato Sauce": {
+      "Tomatoes": null,
+      "Garlic": null
+    },
+    "Bread": null
+  },
+  "Chili": {
+    "Ground Beef": null,
+    "Canned Beans": null,
+    "Tomatoes": null,
+    "Garlic": null,
+    "Onions": null
+  },
+  "Bread Pudding": {
+    "Bread": null,
+    "Sugar": null,
+    "Eggs": null,
+    "Milk": null
+  }
 };
 
 const player2Items = {
-  "ApplePie": {
-    "Apples": null,
-    "Crust": {
-      "Milk": null,
-      "Flour": null
+  "Apple Pie": {
+    "Pie Crust": {
+      "Flour": null,
+      "Butter": null
     },
+    "Apples": null,
     "Sugar": null
   },
-  "Chicken Pot Pie": {
-    "Chicken": null,
-    "Crust": {
-      "Milk": null,
-      "Flour": null
+  "Lasagna": {
+    "Pasta": null,
+    "Tomato Sauce": {
+      "Tomatoes": null,
+      "Garlic": null
     },
-    "A Pot": null
+    "Cheese": null,
+    "Lasagna Filling": {
+      "Ground Beef": null,
+      "Spinach": null
+    }
   },
+  "Tuna Sandwich": {
+    "Bread": null,
+    "Canned Tuna": null,
+    "Mayonnaise": null
+  },
+  "Beef Stew":{
+    "Ground Beef": null,
+    "Potatoes": null,
+    "Carrots": null,
+    "Onions": null
+  },
+  "Carrot Cake": {
+    "Flour": null,
+    "Butter": null,
+    "Sugar": null,
+    "Carrots": null
+  },
+  "Quiche": {
+    "Pie Crust": {
+      "Flour": null,
+      "Butter": null
+    },
+    "Eggs": null,
+    "Cheese": null,
+    "Quiche Filling": {
+      "Spinach": null,
+      "Spam": null
+    }
+  },
+  "Pasta Salad": {
+    "Pasta": null,
+    "Mayonnaise": null
+  }
 };
 
 
